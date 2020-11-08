f8dc1f63f01200519d2bbe1c0ff521237db4b4b7
Luiz <luizgustavofcsg@hotmail.com>
2020-11-08 12:27:08 -0300

    Implementation of return mock data for products

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import products from '../mock_data/products'
 
 const router = Router()
 
 router.get('/', (req, res) => {
     return res.send('test')
+})
+
+router.get('/products', (req, res) => {
+    return res.json(products)
 })
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,6 @@ import router from './routes'
 const app = express()
 
 app.use(router)
+app.use(express.json())
 
 app.listen(8080)
