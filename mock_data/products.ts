d62b0905a9892ca9bd583c848a02403073dddb58
Luiz <luizgustavofcsg@hotmail.com>
2020-11-08 12:23:35 -0300

    Created mocking data for the products

---
+++ mock_data/products.ts
@@ -0,0 +1,40 @@
+const products = [
+    {
+        name: "CPU",
+        description: "Central processing unit",
+        code: 'A0000',
+        measure_type: 'unit'
+    }, 
+    {
+        name: "RAM",
+        description: "Volatile memory",
+        code: 'B0000',
+        measure_type: 'unit'
+    }, 
+    {
+        name: "Hard Drive",
+        description: "Non-volatile memory",
+        code: 'B0001',
+        measure_type: 'unit'
+    }, 
+    {
+        name: "Motherboard",
+        description: "Intercomunication device between components",
+        code: 'C0000',
+        measure_type: 'unit'
+    }, 
+    {
+        name: "Tower",
+        description: "Holder box for the eletronics",
+        code: 'D0000',
+        measure_type: 'unit'
+    },
+    {
+        name: "Computer",
+        description: "Computation device that runs applications",
+        code: 'E0000',
+        measure_type: 'unit'
+    } 
+]
+
+export default products;
