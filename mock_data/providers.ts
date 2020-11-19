bfd6798c33ac1740b1231a682a91b729e4a9acfd
Luiz <luizgustavofcsg@hotmail.com>
2020-11-18 21:09:19 -0300

    Created mocking data for providers

---
+++ mock_data/providers.ts
@@ -0,0 +1,26 @@
+const providers = [
+    {
+        id: "0",
+        name: "DeltaOPT",
+        document: "12345678901",
+        email: "luizgustavofcsg@hotmail.com",
+        phone: "+553223452345"
+    },
+    {
+        id: "1",
+        name: "Luiz",
+        document: "19216000101",
+        email: "email@test.com",
+        phone: "+553223452345"
+    },
+    {
+        id: "2",
+        name: "Company",
+        document: "DL256235",
+        email: "company@email.com",
+        phone: "+553299999999"
+    }
+]
+
+
+export default providers;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import products from '../mock_data/products'
+import providers from '../mock_data/providers'
 
 const router = Router()
 
@@ -9,6 +10,10 @@ router.get('/', (req, res) => {
 
 router.get('/products', (req, res) => {
     return res.json(products)
+})
+
+router.get('/providers', (req, res) => {
+    return res.json(providers)
 })
 
 export default router
